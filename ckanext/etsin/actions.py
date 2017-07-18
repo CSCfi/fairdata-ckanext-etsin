400310b35e878cf8d77430401414aa24d4f63699
Meepu <meeri.panula@gofore.com>
2017-07-18 20:14:50 +0300

    Implement create_package, initial commit

---
+++ ckanext/etsin/actions.py
@@ -0,0 +1,15 @@
+'''
+Action overrides
+'''
+
+# For development use
+import logging
+log = logging.getLogger(__name__)
+
+def package_create(context, data_dict):
+
+    log.info("Greetings from within the Etsin package_create. Here's the dict I'm working with:")
+    log.info(data_dict)
+
+    # Return some dict (doesn't make sense yet)
+    return data_dict
--- ckanext/etsin/plugin.py
+++ ckanext/etsin/plugin.py
@@ -1,9 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.etsin import actions
 
 class EtsinPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IActions)
 
     # IConfigurer
 
@@ -11,3 +13,11 @@ class EtsinPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'etsin')
+
+    # IActions
+
+    def get_actions(self):
+        """ Register actions. """
+        return {
+            'package_create': actions.package_create,
+        }
