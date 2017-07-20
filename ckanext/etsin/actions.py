b941569be2438254cc24162d9b5ae992a54de942
Meepu <meeri.panula@gofore.com>
2017-07-20 21:39:52 +0300

    Add some comments

--- ckanext/etsin/actions.py
+++ ckanext/etsin/actions.py
@@ -7,17 +7,28 @@ import logging
 log = logging.getLogger(__name__)
 
 def package_create(context, data_dict):
+    '''
+    Refines data_dict. Calls Metax API to create new dataset.
+    '''
 
+    # Just for testing
     log.info("Greetings from within the Etsin package_create. Here's the dict I'm working with:")
     log.info(data_dict)
 
-    # Return some dict (doesn't make sense yet)
     return data_dict
 
 
 def package_delete(context, data_dict):
+    '''
+    Calls Metax API to delete a dataset.
+    '''
+
     return
 
 
 def package_update(context, data_dict):
+    '''
+    Refines data_dict. Calls Metax API to update an existing dataset.
+    '''
+
     return data_dict
