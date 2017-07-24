803b97d465b58cee6177ce501e28a712503f15a4
Meepu <meeri.panula@gofore.com>
2017-07-24 19:57:21 +0300

    Add placeholders for API calls

--- ckanext/etsin/actions.py
+++ ckanext/etsin/actions.py
@@ -16,9 +16,9 @@ def package_create(context, data_dict):
     # Refine data_dict based on organization it belongs to
     data_dict = refine(data_dict)
 
-    # Just for testing
-    log.info("Greetings from within the Etsin package_create. Here's the dict I'm working with:")
-    log.info(data_dict)
+    # Check with Metax if we should be creating or updating and do that
+    # TODO: We may need to catch an error here
+    _create_or_update(data_dict)
 
     return data_dict
 
@@ -31,7 +31,14 @@ def package_delete(context, data_dict):
     # TODO: Do we need to refine data_dict here? If there is any refiner that 
     # changes package if, we need to refine here, too.
 
-    return
+    # TODO: This is the function that will be created in CSCETSIN-22
+    if ask_metax_whether_package_exists(data_dict['id']):
+        # Metax says the package exists
+        # TODO: This is the function that will be created in CSCETSIN-21
+        # Also, we may need to catch an error
+        delete_package_via_metax_api(data_dict)
+
+    return True
 
 
 def package_update(context, data_dict):
@@ -42,4 +49,41 @@ def package_update(context, data_dict):
     # Refine data_dict based on organization it belongs to
     data_dict = refine(data_dict)
 
+    # Check with Metax if we should be creating or updating and do that
+    # TODO: We may need to catch an error here
+    _create_or_update(data_dict)
+
     return data_dict
+
+
+def _create_or_update(data_dict):
+
+    # Ask Metax if this dataset exists
+    # TODO: This is the function that will be created in CSCETSIN-22
+    if ask_metax_whether_package_exists(data_dict['id']):
+        # Metax says the package exists
+        # TODO: This is the function that will be created in CSCETSIN-20
+        # Also, we may need to catch an error
+        update_package_via_metax_api(data_dict)
+
+    else:
+        # Metax says the package doesn't exist
+        # TODO: This is the function that will be created in CSCETSIN-19
+        # Also, we may need to catch an error
+        create_package_via_metax_api(data_dict)
+
+
+# TODO: These functions should not be implemented here.
+# I'm only defining them for placeholding purposes.
+# All these functions must log all the API calls we make and all the responses.
+def ask_metax_whether_package_exists(id):
+    pass
+
+def create_package_via_metax_api(data_dict):
+    pass
+
+def update_package_via_metax_api(data_dict):
+    pass
+
+def delete_package_via_metax_api(data_dict):
+    pass
