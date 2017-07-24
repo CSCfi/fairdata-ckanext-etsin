3e84f1601b2eb764e521e8b343e81ab201859732
Meepu <meeri.panula@gofore.com>
2017-07-24 15:32:02 +0300

    Example refiner functions

---
+++ ckanext/etsin/refiners/syke.py
@@ -0,0 +1,15 @@
+'''
+Refine Syke data_dict
+'''
+
+# For development use
+import logging
+log = logging.getLogger(__name__)
+
+# Refines Syke data_dict
+# TODO: Not yet implemented
+def syke_refiner(data_dict):
+
+    package_dict = data_dict
+
+    return package_dict
