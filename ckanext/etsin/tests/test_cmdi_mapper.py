71e4489dd6eb3823157acc62e56f5868fe0f020b
Janne Gröndahl <janne.grondahl@gofore.com>
2017-08-01 10:39:22 +0300

    Move test/example data to seperate folders

--- ckanext/etsin/tests/test_cmdi_mapper.py
+++ ckanext/etsin/tests/test_cmdi_mapper.py
@@ -8,14 +8,15 @@ import helpers
 class TestCmdiMapper(TestCase):
     def testMapping(self):
         context = {}
-        context['xml'] = helpers._get_file_as_string('cmdi_record_example.xml')
+        context['xml'] = helpers._get_file_as_string(
+            'kielipankki_cmdi/cmdi_record_example.xml')
         data_dict = {'package_dict': {}}
 
         metax_dict = cmdi_mapper(context, data_dict)['metax_dict']
-        from pprint import pprint
-        import IPython
-        IPython.embed()
-        pprint(metax_dict)
+        # from pprint import pprint
+        # import IPython
+        # IPython.embed()
+        # pprint(metax_dict)
 
 
 if __name__ == '__main__':
