834a8f0662f6c2d11cf3fec456cdf3796402650f
Janne Gröndahl <janne.grondahl@gofore.com>
2017-08-03 13:51:23 +0300

    Create quick script for testing if a given json file is accepted by MetaX API and debug failing

---
+++ ckanext/etsin/tests/try_metax.py
@@ -0,0 +1,21 @@
+"""
+This script is a temporary quick helper for manually checking if a given
+dataset dictionary object is currently accepted by the MetaX API.
+"""
+import ckanext.etsin.metax_api as api
+import helpers
+import os
+
+if __name__ == '__main__':
+    filename = 'minimum_valid_edited.json'
+    filepath = os.path.join('metax_json_dicts', filename)
+    dataset = helpers._get_json_as_dict('metax_json_dicts/minimum_valid_edited.json')
+    dataset['identifier'] += helpers._create_identifier_ending()
+    print "Posting dataset {} with identifier {}".format(filename, dataset['identifier'])
+    id = api.create_dataset(dataset)
+    print "Dataset was posted and it was assigned id {}".format(id)
+    print "Replacing dataset (with same dataset)"
+    api.replace_dataset(id, dataset)
+    print "Deleting the dataset from MetaX"
+    api.delete_dataset(id)
+    print "Success!"
