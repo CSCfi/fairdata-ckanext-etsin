e48dcde8ffb7d3c1e8ff9fca2281a3aa0a3d4846
Janne Gröndahl <janne.grondahl@gofore.com>
2017-08-17 11:46:54 +0300

    Comment out unfinished actions test

--- ckanext/etsin/tests/test_actions.py
+++ ckanext/etsin/tests/test_actions.py
@@ -16,30 +16,30 @@ class TestActions(TestCase):
         """ Test action package_create with real data.
 
         Note: we use cmdi_mapper, so this is not a true unit test.
+        Note: this test is not finished yet, so it is commented out
         """
-        # with patch('ckan.logic.action.create.package_create') as mock_create:
-        if True:
-            # Create the MetaX data dict
-            xml_string = helpers._get_file_as_string(
-                'kielipankki_cmdi/cmdi_record_example.xml')
-            context = {
-                'xml': xml_string,
-                'model': model,
-                'session': model.Session,
-                'user': "test"
-            }
-            data_dict = {'package_dict': {'id': 123}}
-            data_dict = cmdi_mapper(context, data_dict)
-
-            # TEMP: so that refiner does not get confused
-            data_dict['organization'] = 'kielipankki'
-
-            # Try to create the package
-            actions.package_create(context, data_dict)
-
-            # Check that package creation was attempted
-            ok_(mock_create.called)
-
+#        with patch('ckan.logic.action.create.package_create') as mock_create:
+#            # Create the MetaX data dict
+#            xml_string = helpers._get_file_as_string(
+#                'kielipankki_cmdi/cmdi_record_example.xml')
+#            context = {
+#                'xml': xml_string,
+#                'model': model,
+#                'session': model.Session,
+#                'user': "test"
+#            }
+#            data_dict = {'package_dict': {'id': 123}}
+#            data_dict = cmdi_mapper(context, data_dict)
+#
+#            # TEMP: so that refiner does not get confused
+#            data_dict['organization'] = 'kielipankki'
+#
+#            # Try to create the package
+#            actions.package_create(context, data_dict)
+#
+#            # Check that package creation was attempted
+#            ok_(mock_create.called)
+#
 
 if __name__ == '__main__':
     unittest.main()
