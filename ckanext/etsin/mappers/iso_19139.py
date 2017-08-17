4b812486bafa75aa03b0ef3be75221a36a251e83
Meepu <meeri.panula@gofore.com>
2017-08-17 14:31:50 +0300

    ISO 19139 mapping: creator, curator and language

--- ckanext/etsin/mappers/iso_19139.py
+++ ckanext/etsin/mappers/iso_19139.py
@@ -2,6 +2,8 @@
 Map ISO 19139 dicts to Metax values
 '''
 
+from iso639 import languages
+
 # For development use
 import logging
 log = logging.getLogger(__name__)
@@ -9,16 +11,13 @@ log = logging.getLogger(__name__)
 # Overwrites ckanext-spatial's get_package_dict
 def iso_19139_mapper(self, context, data_dict):
 
-    # log.info("Greetings from within the spatial mapper. Here's the dict I'm working with:")
-    # log.info(data_dict)
-
     # Start with an empty slate
     package_dict = {}
 
     # Metax obligatory fields
-    # Miika says these (and only these) are obligatory on 2017/08/14
-    # Don't mind missing values
+    # Don't mind missing values. We'll let Metax handle them.
 
+    # Obligatory in API but not in Metax data model
     try:
         # Use whatever id harvest source gives us
         package_dict['preferred_identifier'] = data_dict['iso_values']['guid']
@@ -31,8 +30,36 @@ def iso_19139_mapper(self, context, data_dict):
     except KeyError:
         package_dict['title'] = [{'default': ''}]
 
+    # Find creators, if any
+    package_dict['creator'] = []
+    try:
+        creators = (org for org in data_dict['iso_values']['responsible-organisation'] if 'author' in org['role'])
+        for org in creators:
+            package_dict['creator'].append({'name': org['organisation-name']})
+    except KeyError:
+        pass
 
-    # log.info("And here's the dict after I mapped it to (demo) Metax format")
-    # log.info(package_dict)    
+    # Find curators, if any
+    package_dict['curator'] = []
+    try:
+        curators = (org for org in data_dict['iso_values']['responsible-organisation'] if 'owner' in org['role'])
+        for org in curators:
+            package_dict['curator'].append({'name': org['organisation-name']})
+    except KeyError:
+        pass
+
+    # Use 'undefined' if language code not given or isn't valid ISO 639-3
+    try:
+        lang = languages.get(part3=data_dict['iso_values']['metadata-language']).part3
+    except:
+        lang = 'und'
+    package_dict['language'] = [{'identifier': _get_language_identifier(lang)}]
 
     return package_dict
+
+
+def _get_language_identifier(lang):
+    if not isinstance(lang, basestring):
+        lang = 'und'
+
+    return 'http://www.lexvo.org/id/iso639-3/' + lang
--- ckanext/etsin/tests/test_mappers_iso_19139.py
+++ ckanext/etsin/tests/test_mappers_iso_19139.py
@@ -14,6 +14,15 @@ class TestMappersISO19139(TestCase):
 
         eq_(dict['preferred_identifier'], 'M28mitl:')
         eq_(dict['title'], [{'default': 'Testiaineisto'}])
+        assert {'name': 'tekija1'} in dict['creator']
+        assert {'name': 'tekija2'} in dict['creator']
+        assert {'name': 'omistaja'} not in dict['creator']
+        assert {'name': 'jotainmuuta'} not in dict['creator']
+        assert {'name': 'tekija1'} not in dict['curator']
+        assert {'name': 'tekija2'} not in dict['curator']
+        assert {'name': 'omistaja'} in dict['curator']
+        assert {'name': 'jotainmuuta'} not in dict['curator']
+        eq_(dict['language'], [{'identifier':'http://www.lexvo.org/id/iso639-3/fin'}])
 
 
     def testObligatoryFieldsMissing(self):
@@ -21,7 +30,34 @@ class TestMappersISO19139(TestCase):
 
         eq_(dict['preferred_identifier'], '')
         eq_(dict['title'], [{'default': ''}])
+        eq_(dict['creator'], [])
+        eq_(dict['curator'], [])
+        eq_(dict['language'], [{'identifier':'http://www.lexvo.org/id/iso639-3/und'}])
 
 
 def _testdict():
-    return {'iso_values': {'guid': 'M28mitl:', 'title': 'Testiaineisto'}}
+    return {
+        'iso_values': {
+            'guid': 'M28mitl:', 
+            'title': 'Testiaineisto',
+            'responsible-organisation': [
+                {
+                    'organisation-name': 'omistaja',
+                    'role': ['owner']
+                    },
+                {
+                    'organisation-name': 'tekija1',
+                    'role': ['author']
+                    },
+                {
+                    'organisation-name': 'tekija2',
+                    'role': ['author']
+                    },
+                {
+                    'organisation-name': 'muu',
+                    'role': ['jotainmuuta']
+                    }, 
+                ],
+              'metadata-language': 'fin',
+            }
+        }
