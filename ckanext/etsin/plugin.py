62191e538dbc11cf0cbb6fafc15122c720f67829
Juha-Matti Lehtinen <junnu.lehtinen@gmail.com>
2017-09-06 11:22:10 +0300

    CSCMETAX-64: Add IOAIPMHHarvester interface to oaipmh extension and use it for cmdi format. Correspondingly, the interface is implemented in etsin extension's plugin.py, which does the mapping for the cmdi format.

--- ckanext/etsin/plugin.py
+++ ckanext/etsin/plugin.py
@@ -1,9 +1,11 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckanext.etsin.mappers import iso_19139
+from ckanext.etsin.mappers import cmdi
 
 from ckanext.etsin import actions
 from ckanext.spatial.interfaces import ISpatialHarvester
+from ckanext.oaipmh.interfaces import IOAIPMHHarvester
 
 import logging
 log = logging.getLogger(__name__)
@@ -12,8 +14,7 @@ class EtsinPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IActions)
     plugins.implements(plugins.IConfigurer)
     plugins.implements(ISpatialHarvester)
-    # plugins.implements(IOAIPMH) # TODO: This interface doesn't exist yet
-
+    plugins.implements(IOAIPMHHarvester)
 
     # IActions
 
@@ -25,7 +26,6 @@ class EtsinPlugin(plugins.SingletonPlugin):
             'package_update': actions.package_update,
         }
 
-
     # IConfigurer
 
     def update_config(self, config_):
@@ -33,24 +33,19 @@ class EtsinPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic', 'etsin')
 
+    # IOAIPMHHarvester
 
-    # IOAIPMH
-    # TODO: This interface has not been implemented in ckanext-oaipmh yet
-
-    # def get_package_dict(self, context, format, data_dict):
-    #     # OAI-PMH comes in several formats
-    #     if format == "cmdi":        
-    #         return mappers.cmdi_mapper(context, data_dict)
-    #     else:
-    #         # TODO: It would probably be better to raise an error here
-    #         # and inform caller about invalid format
-    #         return False
-
+    def get_oaipmh_package_dict(self, format, xml):
+        # OAI-PMH comes in several formats
+        if format == 'cmdi0571':
+            return cmdi.cmdi_mapper(xml)
+        else:
+            return {}
 
     # ISpatialHarvester
 
     def get_package_dict(self, context, data_dict):
-        return iso_19139.iso_19139_mapper(context, data_dict)
+        return iso_19139.iso_19139_mapper(data_dict)
 
     # This needs to be here - otherwise ckanext-spatial fails silently
     def get_validators(self):
